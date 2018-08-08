de5743360d1f5eca84cf1392d3f7c4eb99d2be0c
tayabsoomro <isoomro.t@gmail.com>
2018-08-08 10:48:04 -0600

    fix typo

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import AnalysisPage from "./Pages/AnalysisPage";
-import SetupPage from "./Pages/Setuppage";
+import SetupPage from "./Pages/SetupPage";
 
 
 if(document.getElementById("content") != null)
