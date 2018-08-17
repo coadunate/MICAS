48898cd2bd9b2f9fd7cdc176a311f0937846973c
tayabsoomro <isoomro.t@gmail.com>
2018-08-17 13:49:31 -0600

    add code functionality to send SMS notification. Fix issue #11

--- server/app/main/utils/notification.py
+++ server/app/main/utils/notification.py
@@ -1,17 +1,34 @@
 # Send emails in Python, converted for Python 3.6 by iCrazyBlaze
-import smtplib
+import smtplib, json
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
+from twilio.rest import Client
+
+import os
+sms_config_path = os.path.join(os.path.dirname(os.path.abspath('~/')),'server/app/config/sms_config.json')
+
+twilio_data = None
+with open(sms_config_path) as json_data_file:
+    twilio_data = json.load(json_data_file)
 
 # Your email details
 fromaddr = "coadunateusask@gmail.com"
 PASSWORD = "PASS_HERE"
 
-def send_sms(alert_name,num_reads,email_address):
+def send_sms(alert_name,num_reads,phone_number):
     """
         Sends a SMS message about the alerts to given phone_number.
     """
-    print("Sent!")
+    client = Client(twilio_data['account_sid'], twilio_data['auth_token'])
+    message = client.messages.create(
+      body='[ALERT] ' + str(alert_name) + ' has been detected in your sample! \
+      There were ' + str(num_reads) + ' found, which is above your set threshold and \
+      therefore, we thought you probably want to take a look at the analysis.',
+      from_=str(twilio_data['from_ph_number']),
+      to=str(phone_number)
+    )
+    print("Message sent successfully, SID: " + str(message.sid))
 
 def send_email(alert_name, num_reads,email_address):
     """
@@ -21,7 +38,7 @@ def send_email(alert_name, num_reads,email_address):
     SUBJECT = "MICAS -- [ALERT] " + str(alert_name) + " was detected in your sample!"
     body = "Hi,\nMICAS has recently detected " + str(num_reads) + " reads of \
     " + str(alert_name)  + " in your sample. It probably needs your attention.\n\
-    Thanks,\n- Your friends at MICAS"
+    Thanks,\nYour friends at MICAS"
 
 
     try:
