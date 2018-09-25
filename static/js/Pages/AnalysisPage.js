6c650201ff1728a4b1c496931f820088c78e9ac5
tayabsoomro <isoomro.t@gmail.com>
2018-09-25 15:03:34 -0600

    recovering from CS50

--- static/js/Pages/AnalysisPage.js
+++ static/js/Pages/AnalysisPage.js
@@ -3,12 +3,14 @@ import {Grid, Row, Col, Well, Button } from 'react-bootstrap';
 import {ListGroup, ListGroupItem} from 'react-bootstrap'
 import { makeWidthFlexible, Sankey } from 'react-vis';
 import HeaderView from '../components/HeaderView';
+import KronaView from '../components/KronaView';
 import Hint from 'react-vis/dist/plot/hint'
 import { LabelSeries } from 'react-vis'
 import {Sunburst} from 'react-vis';
 import { Dot } from 'react-animated-dots';
 import {curveCatmullRom} from 'd3-shape';
 import 'react-vis/dist/style.css';
+import ReactFauxDOM from 'react-faux-dom';
 import {
   XYPlot,
   XAxis,
@@ -19,14 +21,35 @@ import {
   HorizontalBarSeries
 } from 'react-vis'
 
-var $ = require('jquery');
+var margin = { top: 10, right: 0, bottom: 10, left: 0 };
+var width = 690 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
 
+var $ = require('jquery');
 
 import io from 'socket.io-client';
 
 let socket = io('http://' + document.domain + ':' + location.port + '/analysis');
 
 import '../../css/AnalysisPage.css';
+
+import d3 from 'd3';
+import sankey from 'd3-plugins-sankey';
+let _ = require('underscore');
+var cloneDeep = require('lodash.clonedeep');
+
+var color = function(){
+  const color = d3.scale.ordinal(d3.schemeCategory10);
+  return name => color(name.replace(/ .*/, ""));
+}
+
+var my_sankey = d3.sankey()
+  .size([width, height])
+  .nodeWidth(15)
+  .nodePadding(10);
+
+var path = my_sankey.link();
+
 
 
 const BarSeries = HorizontalBarSeries;
@@ -62,11 +85,23 @@ const D3FlareData = {
    "hex": "#12939A",
    "value": 20,
    "children": [
-    {"name": "ALERT SEQUENCES", "hex": "#12939A", "value": 2}
+    {"name": "Proteobacteria", "hex": "#12939A", "value": 5},
+    {
+      "name": "Spirochaetes",
+      "hex": "#12939A",
+      "value": 2,
+      "children": [
+        {"name": "Alert 1", "hex": "#FF8800", "value": 14}
+      ]
+    },
+    {"name": "Fusobacteria", "hex": "#12939A", "value": 7},
+    {"name": "Acidobacteria", "hex": "#12939A", "value": 8},
+    {"name": "Thermodesulfobacteria", "hex": "#12939A", "value": 12},
+    {"name": "Euryarchaeota", "hex": "#12939A", "value": 12},
    ]
   },
   {
-   "name": "UNCLASSIFIED",
+   "name": "UNPROCESSED",
    "value": 50
   }
  ]
@@ -91,7 +126,7 @@ const tipStyle = {
 const boxStyle = {height: '10px', width: '10px'};
 
 const LABEL_STYLE = {
-  fontSize: '8px',
+  fontSize: '15px',
   textAnchor: 'middle'
 };
 
@@ -210,170 +245,242 @@ class AnalysisPage extends Component {
       socket.emit('start_fastq_file_listener',this.props.appLocation,this.props.minionLocation)
 
     }
+    render() {
+      var graph = {
+        nodes: cloneDeep(this.state.sankeyData.nodes),
+        links: cloneDeep(this.state.sankeyData.links)
+      };
 
-    render () {
-        const {activeLink} = this.state;
-        var SankeyPlot = ({ width }) =>
-                <Sankey
-                  style={{
-                    height: "auto",
-                    minHeight: "550%",
-                    paddingTop: "100px"
-                  }}
-                  nodes={this.state.sankeyData.nodes}
-                  links={this.state.sankeyData.links}
-                  width={width}
-                  height={200}
-                  layout={2}
-                  align="left"
-                  hideLabels={false}
-                  labelRotation={-45}
-                  nodePadding={85}
-                  nodeWidth={5}
-                >
-                </Sankey>
+      var getId = (function () {
+        var incrementingId = 0;
+        return function(element) {
+          if (!element.id) {
+            element.id = "id_" + incrementingId++;
+            // Possibly add a check if this ID really is unique
+          }
+          return element.id;
+        };
+      }());
 
-        SankeyPlot.propTypes = { width: React.PropTypes.number }
-        var FlexSankeyPlot = makeWidthFlexible(SankeyPlot)
+      my_sankey.nodes(graph.nodes)
+        .links(graph.links)
+        .layout(32);
 
-        var downloadDatabase = this.state.databaseDownloaded;
 
-        var alertdata = []
+      var svgNode = ReactFauxDOM.createElement('div');
 
-        this.state.alertsInfo.alerts.map( (sequence) => {
-            alertdata.push(<BarSeries key={sequence.name} onValueClick={(datapoint, event)=>{ return (<Hint x={10} y={10} value={ tayab } style={{fontSize: 14}}/>) }} data={[{"y": sequence.name, "x": sequence.num_reads}]} />)
-        })
+      var svg = d3.select(svgNode).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        var thresholdlinedata = []
-        this.state.alertsInfo.alerts.map( (sequence) => {
-          thresholdlinedata.push({"y": sequence.name, "x": this.state.alertsInfo.seqs_threshold })
-        })
 
-        if(downloadDatabase){
-          const {clicked, data, finalValue, pathValue} = this.state;
-          return (
-              <div className="SetupPage">
-                <Grid fluid={true}>
-                  <div className="walk"></div>
-                  <Row className="show-grid">
-                    <Col md={12}>
-                      <Col md={1} />
-                      <Col md={10}>
-                        <HeaderView name={this.props.name}/>
-                        <Col md={12}>
-                            <Col md={6}>
-                              <Well>
-                                <Sunburst
-                                  animation
-                                  className="basic-sunburst-example"
-                                  hideRootNode
-                                  onValueMouseOver={node => {
-                                    if (clicked) {
-                                      return;
-                                    }
-                                    const path = getKeyPath(node).reverse();
-                                    const pathAsMap = path.reduce((res, row) => {
-                                      res[row] = true;
-                                      return res;
-                                    }, {});
-                                    this.setState({
-                                      finalValue: path[path.length - 1],
-                                      pathValue: path.join(' > '),
-                                      data: updateData(decoratedData, pathAsMap)
-                                    });
-                                  }}
-                                  onValueMouseOut={() => clicked ? () => {} : this.setState({
-                                    pathValue: false,
-                                    finalValue: false,
-                                    data: updateData(decoratedData, false)
-                                  })}
-                                  onValueClick={() => this.setState({clicked: !clicked})}
-                                  style={{
-                                    stroke: '#ddd',
-                                    strokeOpacity: 0.3,
-                                    strokeWidth: '0.5'
-                                  }}
-                                  colorType="literal"
-                                  getSize={d => d.value}
-                                  getColor={d => d.hex}
-                                  data={data}
-                                  height={300}
-                                  width={350}>
-                                  {finalValue && <LabelSeries data={[
-                                    {x: 0, y: 0, label: finalValue, style: LABEL_STYLE}
-                                  ]} />}
+      // Add links
+      var link = svg.append("g").selectAll(".link")
+        .data(graph.links)
+        .enter().append("path")
+        .attr("class", "link")
+        .attr("d", path)
+        .style("stroke-width", (d) => Math.max(1, d.dy))
 
-                                </Sunburst>
-                              </Well>
-                            </Col>
-                            <Col md={6}>
-                              <Well>
-                                <div className="basic-sunburst-example-path-name">{ pathValue ? pathValue : "*hover over the timeline*" }</div>
-                                <div style={{ textAlign: "left"}}>
-                                  <b>Alert Sequences:</b> 2
-                                  <br />
-                                  <b>Classified:</b> 20
-                                  <br />
-                                  <b>Unclassified:</b> 50
-                                </div>
-                              </Well>
-                            </Col>
-                        </Col>
-                        <Col md={12}>
-                          <Well>
-                            <h4>Alerts</h4>
-                            <XYPlot
-                              width={800}
-                              height={300}
-                              margin={{left: 190, right: 10, top: 10, bottom: 40}}
-                              yType="ordinal">
-                              <VerticalGridLines />
-                              <HorizontalGridLines />
-                              <XAxis />
-                              <YAxis />
-                              {alertdata}
-                              <LineSeries color="red" data={thresholdlinedata}/>
-                            </XYPlot>
-                          </Well>
-                        </Col>
-                        <Col md={12}>
-                          <div style={{ height: '30vh'}}>
-                            { this.state.sankeyData.nodes.length > 0 &&
-                              <FlexSankeyPlot />
-                            }
-                            {
-                              this.state.sankeyData.nodes.length == 0 &&
-                              <div>
-                                <br />
-                                <h3>No significant data has been reported for snakey plot yet</h3>
-                              </div>
-                            }
-                          </div>
-                        </Col>
-                      </Col>
-                      <Col md={1} />
-                    </Col>
-                  </Row>
-                </Grid>
-              </div>
-          );
-        }else{
-          return (
-            <div>
-              <Col md={12}>
-                <Well style={{top:'0', bottom:'0', left:'0', right:'0', minHeight: window.innerHeight + 'px', position: 'absolute'}}>
-                  <h3 className="text-center" style={{ marginTop: window.innerHeight/2 + 'px'}}>
-                    The database is still downloading
-                    <Dot><span style={{ fontSize: '40px' }} >.</span></Dot>
-                    <Dot><span style={{ fontSize: '40px' }} >.</span></Dot>
-                    <Dot><span style={{ fontSize: '40px' }} >.</span></Dot>
-                  </h3>
-                </Well>
-              </Col>
-            </div>
-          );
-        }
+
+      // add link titles
+      link.append("title")
+        .text((d) => d.source.name + " → " + d.target.name + "\n Weight: " + d.value);
+
+
+      // Add nodes
+      var node = svg.append("g").selectAll(".node")
+        .data(graph.nodes)
+        .enter().append("g")
+        .attr("class", "node")
+        .attr("fill",(d) => color(d.name))
+        .attr("transform", (d) => "translate(" + d.x + "," + d.y + ")")
+
+      // add nodes rect
+      node.append("rect")
+        .attr("height", (d) => d.dy)
+        .attr("width", my_sankey.nodeWidth())
+        .append("title")
+        .text((d) => d.name + "\n" + d.value);
+
+      // add nodes text
+      node.append("text")
+        .attr("x", -6)
+        .attr("y", (d) => d.dy / 2)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "end")
+        .text((d) => d.name)
+        .filter((d) => d.x < width / 2)
+        .attr("x", 6 + my_sankey.nodeWidth())
+        .attr("text-anchor", "start");
+
+
+      return svgNode.toReact();
     }
+    // render () {
+    //     const {activeLink} = this.state;
+    //
+    //     var SankeyPlot = ({ width }) =>
+    //             <Sankey
+    //               style={{
+    //                 height: "auto",
+    //                 minHeight: "550%",
+    //                 paddingTop: "100px"
+    //               }}
+    //               nodes={this.state.sankeyData.nodes}
+    //               links={this.state.sankeyData.links}
+    //               width={width}
+    //               height={200}
+    //               layout={2}
+    //               align="center"
+    //               hideLabels={false}
+    //               labelRotation={-45}
+    //               nodePadding={2}
+    //               nodeWidth={5}
+    //             >
+    //             </Sankey>
+    //
+    //     SankeyPlot.propTypes = { width: React.PropTypes.number }
+    //     var FlexSankeyPlot = makeWidthFlexible(SankeyPlot)
+    //
+    //     var downloadDatabase = this.state.databaseDownloaded;
+    //
+    //     var alertdata = []
+    //
+    //     this.state.alertsInfo.alerts.map( (sequence) => {
+    //         alertdata.push(<BarSeries key={sequence.name} onValueClick={(datapoint, event)=>{ return (<Hint x={10} y={10} value={ tayab } style={{fontSize: 14}}/>) }} data={[{"y": sequence.name, "x": sequence.num_reads}]} />)
+    //     })
+    //
+    //     var thresholdlinedata = []
+    //     this.state.alertsInfo.alerts.map( (sequence) => {
+    //       thresholdlinedata.push({"y": sequence.name, "x": this.state.alertsInfo.seqs_threshold })
+    //     })
+    //
+    //     if(downloadDatabase){
+    //       const {clicked, data, finalValue, pathValue} = this.state;
+    //       return (
+    //
+    //         <div className="SetupPage">
+    //
+    //             <Grid fluid={true}>
+    //               <div className="walk"></div>
+    //               <Row className="show-grid">
+    //                 <Col md={12}>
+    //                   <Col md={1} />
+    //                   <Col md={10}>
+    //                     <HeaderView name={this.props.name}/>
+    //                   </Col>
+    //                   <Col md={1} />
+    //                 </Col>
+    //               </Row>
+    //               <Row>
+    //                 <Col md={12}>
+    //                   <Well style={{ minHeight: "300px"}}>
+    //                     <Col md={4}></Col>
+    //                     <Col md={4}>
+    //                       <Sunburst
+    //                         animation
+    //                         className="basic-sunburst-example"
+    //                         hideRootNode
+    //                         onValueMouseOver={node => {
+    //                           if (clicked) {
+    //                             return;
+    //                           }
+    //                           const path = getKeyPath(node).reverse();
+    //                           const pathAsMap = path.reduce((res, row) => {
+    //                             res[row] = true;
+    //                             return res;
+    //                           }, {});
+    //                           this.setState({
+    //                             finalValue: path[path.length - 1],
+    //                             pathValue: path.join(' > '),
+    //                             data: updateData(decoratedData, pathAsMap)
+    //                           });
+    //                         }}
+    //                         onValueMouseOut={() => clicked ? () => {} : this.setState({
+    //                           pathValue: false,
+    //                           finalValue: false,
+    //                           data: updateData(decoratedData, false)
+    //                         })}
+    //                         onValueClick={() => this.setState({clicked: !clicked})}
+    //                         style={{
+    //                           stroke: '#ddd',
+    //                           strokeOpacity: 0.3,
+    //                           strokeWidth: '0.5'
+    //                         }}
+    //                         colorType="literal"
+    //                         getSize={d => d.value}
+    //                         getColor={d => d.hex}
+    //                         data={data}
+    //                         height={300}
+    //                         width={350}>
+    //                         {finalValue && <LabelSeries data={[
+    //                           {x: 0, y: 0, label: finalValue, style: LABEL_STYLE}
+    //                         ]} />}
+    //
+    //                       </Sunburst>
+    //                     </Col>
+    //                     <Col md={4}></Col>
+    //                   </Well>
+    //                 </Col>
+    //               </Row>
+    //               <Row>
+    //                 <Col md={12}>
+    //                   <Well>
+    //                     <h4>Alerts</h4>
+    //                     <XYPlot
+    //                       width={780}
+    //                       height={300}
+    //                       margin={{left: 180, right: 10, top: 10, bottom: 40}}
+    //                       yType="ordinal">
+    //                       <VerticalGridLines />
+    //                       <HorizontalGridLines />
+    //                       <XAxis />
+    //                       <YAxis />
+    //                       {alertdata}
+    //                       <LineSeries color="red" data={thresholdlinedata}/>
+    //                     </XYPlot>
+    //                   </Well>
+    //                 </Col>
+    //               </Row>
+    //               <Row>
+    //                 <Col md={12}>
+    //                   <div style={{ height: '30vh'}}>
+    //                     { this.state.sankeyData.nodes.length > 0 &&
+    //                       <FlexSankeyPlot />
+    //                     }
+    //                     {
+    //                       this.state.sankeyData.nodes.length == 0 &&
+    //                       <div>
+    //                         <br />
+    //                         <h3>No significant data has been reported for snakey plot yet</h3>
+    //                       </div>
+    //                     }
+    //                   </div>
+    //                 </Col>
+    //               </Row>
+    //             </Grid>
+    //           </div>
+    //       );
+    //     }else{
+    //       return (
+    //         <div>
+    //           <Col md={12}>
+    //             <Well style={{top:'0', bottom:'0', left:'0', right:'0', minHeight: window.innerHeight + 'px', position: 'absolute'}}>
+    //               <h3 className="text-center" style={{ marginTop: window.innerHeight/2 + 'px'}}>
+    //                 The database is still downloading
+    //                 <Dot><span style={{ fontSize: '40px' }} >.</span></Dot>
+    //                 <Dot><span style={{ fontSize: '40px' }} >.</span></Dot>
+    //                 <Dot><span style={{ fontSize: '40px' }} >.</span></Dot>
+    //               </h3>
+    //             </Well>
+    //           </Col>
+    //         </div>
+    //       );
+    //     }
+    // }
 }
 
 export default AnalysisPage;
