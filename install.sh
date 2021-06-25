1cb244505ca234fbf5cbd7f3014f73fbd06b6243
Sam Horovatin <shorovatin@MacBook-Pro.hitronhub.home>
2021-06-25 15:57:19 -0600

    Streamlined script, made it easier to add new steps

--- install.sh
+++ install.sh
@@ -22,34 +22,24 @@ fatal_error() {
 }
 
 ENVIRONMENT_CMD=""
-
 DEBUG=2
+STEP=1
 
 #################
 ## MAIN SCRIPT ##
 #################
 
-debug "Step 1: Figuring out environment command"
-#if command -v virtualenv &>/dev/null; then
-#  ENVIRONMENT_CMD=virtualenv
-#  debug "Envrionment command: virtualenv"
+debug "Step $STEP: Figuring out environment command"
 if command -v conda &>/dev/null; then
   ENVIRONMENT_CMD=conda
   debug "Environment command: conda"
 else
   fatal_error "Conda is needed to run MICAS"
 fi
+STEP=$(($STEP+1))
 
-debug "Step 2: Creating server environment"
-if [ "$ENVIRONMENT_CMD" = "virtualenv" ]; then
-
-  create_virtual_env_cmd="virtualenv -p python3 $(date)/server/micas_env"
-  debug "$create_virtual_env_cmd"
-  echo "$create_virtual_env_cmd" | bash
-
-  source "$(dirname "$0")"/server/micas_env/bin/activate
-
-elif [ "$ENVIRONMENT_CMD" = "conda" ]; then
+debug "Step $STEP: Creating server environment"
+if [ "$ENVIRONMENT_CMD" = "conda" ]; then
   create_conda_env_cmd="conda create -n micas_env python=3.6 -y -q"
   debug "$create_conda_env_cmd"
   echo "$create_conda_env_cmd" | bash
@@ -57,97 +47,84 @@ else
   fatal_error "No environment command found" \
     "Make sure that you have either virtualenv or conda installed"
 fi
+STEP=$(($STEP+1))
 
-debug "Step 3: Sourcing to the environment"
-if [ "$ENVIRONMENT_CMD" = "virtualenv" ]; then
+debug "Step $STEP: Sourcing to the environment"
 
-  src_cmd="source $(dirname "$0")/server/micas_env/bin/activate"
-  debug "$src_cmd"
-  echo "$src_cmd" | bash
+eval "$($(which conda)  'shell.bash' 'hook')"
+conda activate "micas_env"
+debug "Now in $CONDA_DEFAULT_ENV"
+STEP=$(($STEP+1))
 
-elif [ "$ENVIRONMENT_CMD" = "conda" ]; then
-  # activate_conda_cmd="conda activate micas_env"
-  # init_conda_cmd="conda init --reverse --all"
-  eval "$($(which conda)  'shell.bash' 'hook')"
-  conda activate "micas_env"
-  debug "Now in $CONDA_DEFAULT_ENV"
-fi
-
-debug "Step 4: Installing server dependencies"
+debug "Step $STEP: Selecting download command"
 if command -v curl &>/dev/null; then
-  curl_cmd="curl https://bootstrap.pypa.io/get-pip.py | python3"
-  debug "$curl_cmd"
-  echo "$curl_cmd" | bash
+  URL_CMD="curl"
 elif command -v wget &>/dev/null; then
-  wget_cmd="wget https://bootstrap.pypa.io/get-pip.py"
-  debug "$wget_cmd"
-  echo "$wget_cmd" | bash
-
-  pip_cmd="python3 get-pip.py"
-  debug "$pip_cmd"
-  echo "$pip_cmd" | bash
+  URL_CMD="wget"
 else
-  fatal_error "You need either curl or wget for this"
+  fatal_error "curl or wget is required to continue"
 fi
+STEP=$(($STEP+1))
 
-debug "Step 5: Installing Setup tools"
+debug "Step $STEP: Installing server dependencies"
+if [ "$URL_CMD" = "curl" ]; then
+  pip_get_cmd="$URL_CMD https://bootstrap.pypa.io/get-pip.py | python3"
+  debug "$pip_get_cmd"
+  echo "$pip_get_cmd"| bash
+else
+  pip_get_cmd="$URL_CMD https://bootstrap.pypa.io/get-pip.py"
+  debug "$pip_get_cmd"
+  echo "$pip_get_cmd"| bash
+fi
+STEP=$(($STEP+1))
+
+debug "Step $STEP: Installing setup tools"
 setup_tools_cmd="pip install --upgrade setuptools"
 debug "$setup_tools_cmd"
 echo "$setup_tools_cmd" | bash
+STEP=$(($STEP+1))
 
-debug "Step 6: Installing Python requirements"
+debug "Step $STEP: Installing python requirements"
 req_cmd="pip3 install -r $(dirname "$0")/server/requirements.txt --ignore-installed PyYAML"
 debug "$req_cmd"
 echo "$req_cmd" | bash
+STEP=$(($STEP+1))
 
-debug "Step 7: Installing frontend dependencies"
+debug "Step $STEP: Installing frontend dependencies"
 fend_dep_cmd="npm install --prefix $(dirname "$0")/static $(dirname "$0")/static"
 debug "$fend_dep_cmd"
 echo "$fend_dep_cmd" | bash
+STEP=$(($STEP+1))
 
-debug "Step 8: Building the front-end ReactJS application"
+debug "Step $STEP: Building the front-end reactJS application"
 build_cmd="npm run build --prefix $(dirname "$0")/static"
 echo "$build_cmd" | bash
+STEP=$(($STEP+1))
 
-debug "Step 9: Installing redis"
+debug "Step $STEP: Installing redis"
 if command -v redis-server &>/dev/null; then
   debug "Redis is installed"
 else
-  if command -v wget &>/dev/null; then
-    curl_dload_cmd="wget http://download.redis.io/redis-stable.tar.gz"
-    debug "$curl_dload_cmd"
-    echo "$curl_dload_cmd" | bash
-
-  elif command -v curl &>/dev/null; then
-    wget_dload_cmd="curl http://download.redis.io/redis-stable.tar.gz"
-    debug "$wget_dload_cmd"
-    echo "$wget_dload_cmd" | bash
-
+  if [ "$URL_CMD" = "curl" ]; then
+    url_dload_cmd="$URL_CMD -L http://download.redis.io/redis-stable.tar.gz | tar zx"
+  else
+    url_dload_cmd="$URL_CMD -O - http://download.redis.io/redis-stable.tar.gz | tar zx"
   fi
-
-  ext_cmd="tar -xzvf redis-stable.tar.gz"
-  debug "$ext_cmd"
-  echo "$ext_cmd" | bash
+  debug "$url_dload_cmd"
+  echo "$url_dload_cmd" | bash
 fi
+STEP=$(($STEP+1))
 
-debug "Step 10: Checking if centrifuge is installed"
+debug "Step $STEP: Checking if centrifuge is installed"
 if hash centrifuge-build; then
   debug "Centrifuge is installed"
 else
-  if command -v wget &>/dev/null; then
-    wget_dload_cent_cmd="wget https://github.com/infphilo/centrifuge/archive/v1.0.3.tar.gz"
-    debug "$wget_dload_cent_cmd"
-    echo "$wget_dload_cent_cmd" | bash
-
-  elif command -v curl &>/dev/null; then
-    curl_dload_cent_cmd="curl https://github.com/infphilo/centrifuge/archive/v1.0.3.tar.gz"
-    debug "$curl_dload_cent_cmd"
-    echo "$curl_dload_cent_cmd" | bash
-    curl https://github.com/infphilo/centrifuge/archive/v1.0.3.tar.gz
+  if [ "$URL_CMD" = "curl" ]; then
+    url_dload_cent_cmd="$URL_CMD -L https://codeload.github.com/infphilo/centrifuge/tar.gz/v1.0.3 | tar zx"
+  else
+    url_dload_cent_cmd="$URL_CMD -O - https://codeload.github.com/infphilo/centrifuge/tar.gz/v1.0.3 | tar zx"
   fi
-
-  ext_cent_cmd="tar -xzvf v1.0.3.tar.gz"
-  debug "$ext_cent_cmd"
-  echo "$ext_cent_cmd" | bash
-
+  debug "$url_dload_cent_cmd"
+  echo "$url_dload_cent_cmd" | bash
 fi
+STEP=$(($STEP+1))
