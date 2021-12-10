23b49f6c4b8586a1ffac7200b98fd891357753f2
tayabsoomro <isoomro.t@gmail.com>
2021-12-10 08:32:02 -0400

    remove unnecessary code for downloading the taxonomy files and generating an NCBI tax id

--- server/app/main/utils/tasks.py
+++ server/app/main/utils/tasks.py
@@ -4,14 +4,11 @@ from celery.exceptions import SoftTimeLimitExceeded
 from flask_socketio import emit
 import json
 
-from Bio import Entrez
 import logging
 
 logger = logging.getLogger()
 
 celery = Celery('tasks', broker='redis://localhost', backend='redis')
-
-Entrez.email = 'tayab.soomro@usask.ca'
 
 
 @celery.task(bind=True, name='app.main.tasks.int_download_database')
@@ -29,111 +26,11 @@ def int_download_database(self, db_data, queries):
     # Create variables for database.
     app_location_database = app_location + 'database/'
 
-    # Download taxonomy
-    print("DOWNLOAD DATABASE: Downloading taxonomy files")
-    self.update_state(
-        state="PROGRESS",
-        meta={'percent-done': 10, 'message': 'Downloading taxonomy files', 'project_id': project_id}
-    )
-
-    tax_cmd = ['centrifuge-download -o ' + app_location_database + 'taxonomy taxonomy']
-    try:
-        tax_cmd_output = subprocess.Popen(tax_cmd, shell=True, stderr=subprocess.STDOUT)
-        tax_cmd_output.communicate()
-        tax_cmd_output.wait()
-    except (OSError, subprocess.CalledProcessError) as exception:
-        print(str(exception))
-        self.update_state(
-            state="FAILURE",
-            meta={'percent-done': 10, 'message': 'ERROR: ' + str(exception), 'project_id': project_id}
-        )
-        return "ER0"
-    else:
-        print("DOWNLOAD DATABASE: Successfully downloaded taxonomy files")
-
-    # Create seqid2taxid.map file
-
-    seqid2taxid_file = open(app_location_database + 'seqid2taxid.map', 'a+')
-    seqid2taxid_file.write('\n')
-
-    print("DOWNLOAD DATABASE: Created seqid2taxid.map file")
-    self.update_state(
-        state="PROGRESS",
-        meta={'percent-done': 20, 'message': 'Creating seqid2taxid.map file', 'project_id': project_id}
-    )
-
     if len(queries) == 0:
         print("DOWNLOAD DATABASE: No queries provided, skipping.")
     else:
-        print("DOWNLOAD DATABASE: Entering queries into taxonomy files.")
-        self.update_state(
-            state="PROGRESS",
-            meta={'percent-done': 30, 'message': 'Entering queries into taxonomy files.', 'project_id': project_id}
-        )
         for i, query in enumerate(queries):
             query_file = open(query['file'], 'r')
-
-            # Generate non-existant tax_id from the given parent tax_id
-            proposed_tax_id = ''
-            already_seen_tax_id = []
-            while True:
-                randNum = random.randint(100, 999)
-                proposed_tax_id = query['parent'] + str(randNum)
-                record = Entrez.read(Entrez.efetch(db='Taxonomy', id=proposed_tax_id, retmode='xml'))
-
-                if len(record) == 0 and proposed_tax_id not in already_seen_tax_id:
-                    already_seen_tax_id.append(proposed_tax_id)
-                    break
-
-            NCBI_id = "sequence_" + str(proposed_tax_id)  # Sequence ID for seqid2taxid.map file
-
-            data = None
-            with open(app_location + 'alertinfo.cfg') as alert_fs:
-                data = json.loads(alert_fs.read())
-                data["queries"][i]["tax_id"] = proposed_tax_id
-
-            with open(app_location + 'alertinfo.cfg', 'w') as write_file:
-                write_file.write(json.dumps(data))
-
-            # Add an entry in seqid2taxid with NCBI's id and the tax_id
-            print("DOWNLOAD_DATABASE: Added " + query['name'] + " entry in seqid2taxid.map file")
-            self.update_state(
-                state="PROGRESS",
-                meta={
-                    'percent-done': 35,
-                    'message': "Added " + query['name'] + " entry in seqid2taxid.map file",
-                    'project_id': project_id
-                }
-            )
-            seqid2taxid_file.write(str(NCBI_id) + '\t' + str(proposed_tax_id) + '\n')
-
-            # Add an entry in nodes.dmp with parent tax_id, tax_id and rank (species)
-            entry = str(proposed_tax_id) + '\t|\t' + str(query['parent']) + '\t|\t' + 'species\t|\n'
-            with open(app_location_database + 'taxonomy/nodes.dmp', 'a+') as nodes_dmp:
-                nodes_dmp.write(entry)
-            print("DOWNLOAD_DATABASE: Added " + query['name'] + " entry in nodes.map file")
-            self.update_state(
-                state="PROGRESS",
-                meta={
-                    'percent-done': 40,
-                    'message': "Added " + query['name'] + " entry in nodes.dmp file",
-                    'project_id': project_id
-                }
-            )
-
-            # Add an entry in names.dmp with tax_id, sci_name, empty unique name, name_class (scientific name)
-            entry = str(proposed_tax_id) + '\t|\t' + str(query['name']) + '\t|\t \t|\t' + 'scientific name \t|\n'
-            with open(app_location_database + 'taxonomy/names.dmp', 'a+') as names_dmp:
-                names_dmp.write(entry)
-            print("DOWNLOAD_DATABASE: Added " + query['name'] + " entry in names.map file")
-            self.update_state(
-                state="PROGRESS",
-                meta={
-                    'percent-done': 45,
-                    'message': "Added " + query['name'] + " entry in names.dmp file",
-                    'project_id': project_id
-                }
-            )
 
             # Putting all the query sequences in one, input_sequences file.
             with open(app_location_database + 'input_sequences.fa', 'a+') as input_sequences:
