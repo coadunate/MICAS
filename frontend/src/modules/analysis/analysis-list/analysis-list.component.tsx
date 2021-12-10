afb1ea06b12a0ec76b7f896ed81dd053617da39d
tayabsoomro <isoomro.t@gmail.com>
2021-12-10 19:00:21 -0400

    update the front-end for listing out the all the analyses

--- frontend/src/modules/analysis/analysis-list/analysis-list.component.tsx
+++ frontend/src/modules/analysis/analysis-list/analysis-list.component.tsx
@@ -1,37 +1,113 @@
-import React from "react";
+import React, {FunctionComponent, useEffect, useState} from "react";
 import {ListGroup} from "react-bootstrap";
-import {Link} from 'react-router-dom';
+import {Link} from "react-router-dom";
+import axios from "axios";
+
+type IAnalysisMetaData = {
+    id: string,
+    minion_dir: string,
+    micas_dir: string
+}
+
+type IListRendererProps = {
+    analyses: IAnalysisMetaData[]
+}
+
+const initial_analyses_data = [
+    {id: "", minion_dir: "", micas_dir: ""}
+];
+
+const ListRenderer: FunctionComponent<IListRendererProps> = ({analyses}) => {
+    return (
+        <div>
+            {
+                analyses.map((a, i) => {
+                    return (
+                        <ListGroup as="ol" className={"numbered pb-2"}>
+                            <ListGroup.Item
+                                as="li"
+                                className="d-flex justify-content-between align-items-start"
+                            >
+                                <div className="ms-2 me-auto">
+                                    <div><b>ID:</b> {a.id}</div>
+                                    <b>MinION Reads
+                                       Directory:</b> {a.minion_dir}
+                                    <br/>
+                                    <b>MICAS Directory:</b> {a.micas_dir}
+                                </div>
+                                <div className={"align-self-center"}>
+                                    <Link to={"/analysis/" + a.id}>
+                                        <button
+                                            className="btn btn-primary mr-3">
+                                            Enter
+                                        </button>
+                                    </Link>
+                                    <button className="btn btn-danger mr-3"
+                                            onClick={() => alert("Feature under development")}>
+                                        Delete
+                                    </button>
+                                </div>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    );
+                })
+            }
+        </div>
+    );
+};
 
 const AnalysisListComponent = () => {
+
+    const [analyses, setAnalyses] = useState(initial_analyses_data);
+    const [status, setStatus]     = useState("loading");
+
+    const getAllAnalyses = () => {
+        return axios({
+            method: "GET",
+            url   : "http://localhost:5000/get_all_analyses"
+        });
+    };
+
+    useEffect(() => {
+        (async () => {
+            const res = await getAllAnalyses();
+            console.log(res);
+
+            if (res.data.status === 200) { // if locations are valid
+                setAnalyses(res.data.data);
+                console.log(res.data.data);
+                if (res.data.data.length > 0) {
+                    setStatus("loaded");
+                } else {
+                    setStatus("notfound");
+                }
+            }
+        })();
+    }, []);
+
+    let RenderObject = <div>404</div>;
+    switch (status) {
+        case "loading":
+            RenderObject = <div>Loading...</div>;
+            break;
+        case "loaded":
+            RenderObject = <ListRenderer analyses={analyses} />
+            break;
+        case "notfound":
+            RenderObject = <div>Not found.</div>
+            break;
+        default:
+            RenderObject = <div>404</div>
+            break;
+    }
+
     return (
         <div className="container">
             <h2>Current Analyses</h2>
-            <div className="vspacer-20" />
-            <ListGroup as="ol" className={"numbered"}>
-                <ListGroup.Item
-                    as="li"
-                    className="d-flex justify-content-between align-items-start"
-                >
-                    <div className="ms-2 me-auto">
-                        <div className="fw-bold">ID: n5ytZ</div>
-                        MinION Reads Directory: /Users
-                        <br/>
-                        MICAS Directory: /Users
-                    </div>
-                    <div>
-                        <Link to="/analysis/n5ytZ">
-                            <button className="btn btn-primary mr-3">
-                                Enter
-                            </button>
-                        </Link>
-                        <button className="btn btn-danger mr-3" onClick={() => alert("Feature under development")}>
-                            Delete
-                        </button>
-                    </div>
-                </ListGroup.Item>
-            </ListGroup>
+            <div className="vspacer-20"/>
+            {RenderObject}
         </div>
-);
+    );
 };
 
 export default AnalysisListComponent;
