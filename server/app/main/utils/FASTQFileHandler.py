58eeafc51a086c11ba13e3dd53a7dd310fa7625a
tayabsoomro <isoomro.t@gmail.com>
2021-12-27 15:48:01 -0600

    add functionality to send email when the threshold exceeds

--- server/app/main/utils/FASTQFileHandler.py
+++ server/app/main/utils/FASTQFileHandler.py
@@ -1,13 +1,12 @@
+import json
+import logging
+import os
+import subprocess
+import sys
+
 from watchdog.events import FileSystemEventHandler
 
-from flask_socketio import emit
-from ... import socketio
-
-import os, subprocess, sys
-
-from .parse import krakenParse
-
-import logging, time
+from .notification import send_email
 
 logger = logging.getLogger()
 
@@ -55,34 +54,64 @@ class FASTQFileHandler(FileSystemEventHandler):
             print("COMMAND OUTPUT: " + str(output), "DEBUG")
 
             # if running minimap2 was successful
-            print("Running minimap2 was successful")
-            # if the final output files already exist, append to them
-            if os.path.isfile(final_output):
-                print(f"{final_output} file exits, appending onto it")
-
-                open(final_output, "a+").writelines([l for l in open(minimap2_output).readlines()])
-
-                subprocess.call(['rm', minimap2_output])
-
-            # if final output files do not exist, move the already
-            # generated output files to the appropriate location,
-            # also rename them as final output respectively.
+            if os.path.getsize(minimap2_output) <= 0:
+                return
             else:
-                subprocess.call(['mv', minimap2_output, final_output])
-                print(
-                    "Renaming the minimap2_output and minimap2_report files to final_output and "
-                    "respectively "
-                )
 
-            # increase the # of files it has classified
-            self.num_files_classified += 1
+                # see which alert sequence this new data matches to and update its percent match value
+                # get the name of the header of alert sequence it matches to
+                minimap2_output_line = os.popen('awk "{print $1}" ' + minimap2_output).read().strip().split("\t")
+                match_alert_header = minimap2_output_line[5]
 
-            # Get the number of files that are in MinION reads directory
-            minion_reads_dir = os.path.abspath(os.path.join(event.src_path, os.pardir))
-            num_files_minion_reads = int(os.popen('ls -1 ' + minion_reads_dir + ' | wc -l').read())
+                # Calculate the new percent threshold
+                num_match = int(minimap2_output_line[9])
+                tot_len = int(minimap2_output_line[6])
+                percent_match_value = (num_match / tot_len) * 100
 
+                # Add the new calculated value to the alert threshold
+                alertinfo_cfg_file = os.path.join(self.app_loc, 'alertinfo.cfg')
+                alertinfo_cfg_data = json.load(open(alertinfo_cfg_file))
+                logger.debug(alertinfo_cfg_data)
+                queries = alertinfo_cfg_data["queries"]
 
-            # TODO: Write a python code block which takes the ($10/$7)*100 from PAF file and writes it into the
-            # {micas_data_dir}/alertinfo.cfg
+                for query in queries:
+                    if query["header"] == match_alert_header:
+                        logger.debug(query)
+                        query["current_value"] = percent_match_value
 
-            # TODO: Email if above percent exceeds the threshold
+                        print("Threshold: ", query["threshold"])
+                        print("PercentMatchValue: ", percent_match_value)
+                        print(query)
+
+                        # get ready to send an alert if needed
+                        if float(query["threshold"]) <= float(percent_match_value):
+                            # send out an email if the percent match exceeds the threshold
+                            send_email(query, alertinfo_cfg_data["email"])
+
+                json.dump(alertinfo_cfg_data, open(alertinfo_cfg_file, 'w'))
+
+                print("Running minimap2 was successful")
+                # if the final output files already exist, append to them
+                if os.path.isfile(final_output):
+                    print(f"{final_output} file exits, appending onto it")
+
+                    open(final_output, "a+").writelines([l for l in open(minimap2_output).readlines()])
+
+                    subprocess.call(['rm', minimap2_output])
+
+                # if final output files do not exist, move the already
+                # generated output files to the appropriate location,
+                # also rename them as final output respectively.
+                else:
+                    subprocess.call(['mv', minimap2_output, final_output])
+                    print(
+                        "Renaming the minimap2_output and minimap2_report files to final_output and "
+                        "respectively "
+                    )
+
+                # increase the # of files it has classified
+                self.num_files_classified += 1
+
+                # Get the number of files that are in MinION reads directory
+                minion_reads_dir = os.path.abspath(os.path.join(event.src_path, os.pardir))
+                num_files_minion_reads = int(os.popen('ls -1 ' + minion_reads_dir + ' | wc -l').read())
