6f20324258fd834bce0b3a33701a40819f637f48
tayabsoomro <isoomro.t@gmail.com>
2021-12-27 15:45:26 -0600

    clean up send_email method and remove redundant send_sms method

--- server/app/main/utils/notification.py
+++ server/app/main/utils/notification.py
@@ -1,56 +1,42 @@
 # Send emails in Python, converted for Python 3.6 by iCrazyBlaze
-import smtplib, json
+import json
+import os
+import logging
+import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-from twilio.rest import Client
-import socketio
-import os
-sms_config_path = os.path.join(os.path.dirname(os.path.abspath('~/')),'server/app/config/sms_config.json')
-email_config_path = os.path.join(os.path.dirname(os.path.abspath('~/')),'server/app/config/email_config.json')
-
-import logging
+email_config_path = os.path.join(os.path.dirname(os.path.abspath('~/')), 'server/app/config/email_config.json')
 
 logger = logging.getLogger()
 
-twilio_data = None
-try:
-   with open(sms_config_path) as json_sms_file:
-       twilio_data = json.load(json_sms_file)
-except:
-   logger.warning("Configuration not set")
-
 email_data = None
+
 try:
     with open(email_config_path) as json_email_file:
         email_data = json.load(json_email_file)
 except:
-    logger.warning("Configuration not set")
+    logger.warning("Email configuration not set")
 
-def send_sms(alert_name,num_reads,phone_number):
-    """
-        Sends a SMS message about the alerts to given phone_number.
-    """
-    client = Client(twilio_data['account_sid'], twilio_data['auth_token'])
-    message = client.messages.create(
-      body='[ALERT] ' + str(alert_name) + ' has been detected in your sample! \
-      There were ' + str(num_reads) + ' found, which is above your set threshold and \
-      therefore, we thought you probably want to take a look at the analysis.',
-      from_=str(twilio_data['from_ph_number']),
-      to=str(phone_number)
-    )
-    print("Message sent successfully, SID: " + str(message.sid))
 
-def send_email(alert_name, num_reads, email_address):
+def send_email(query, email_address):
     """
-        Sends an email about the alerts to given email_address.
+    Send email about query alerts to specified email
+    @param query: the query information to send with the email
+    @param email_address: the email to send the query alert to
     """
+
+    query_name = query["name"]
+    query_value = query["current_value"]
+
     # Email message
-    SUBJECT = "MICAS -- [ALERT] " + str(alert_name) + " was detected in your sample!"
-    body = "Hi,\nMICAS has recently detected " + str(num_reads) + " reads of \
-    " + str(alert_name)  + " in your sample. It probably needs your attention.\n\
-    Thanks,\nYour friends at MICAS"
-
+    SUBJECT = "MICAS -- [ALERT] " + str(query_name) + " was detected in your sample!"
+    body = "Hi,\n\nMICAS has recently detected " + str(query_value) + "% of " + str(query_name) + "in your sample. It " \
+                                                                                                "probably needs your " \
+                                                                                                "attention.\n\n" \
+                                                                                                "Thanks," \
+                                                                                                "\nYour friends at " \
+                                                                                                "MICAS"
 
     try:
         msg = MIMEMultipart()
