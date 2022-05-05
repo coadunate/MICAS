aa887d82b0dd21878bee1c83a1df97c340c70a9d
Tayab Soomro <tayab.soomro@dal.ca>
2022-05-05 17:52:01 -0600

    allow unauthroized certificates for testing purposes

--- frontend/src/app.component.tsx
+++ frontend/src/app.component.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import './app.component.css'
 import RoutesComponent from "./modules/routes/routes.component";
 
-export const io = require('socket.io-client');
-export const socket = io('http://localhost:5000/', { transports: ['websocket','polling']})
+export const socket = require('socket.io-client')('http://0.0.0.0:5000',{rejectUnauthorized: false});
+// export const socket = io('http://0.0.0.0:5000/', { transports: ['websocket','polling']})
 
 socket.on('connect_error', (err: { message: any; }) => {
     console.log(`connect error due to ${err.message}`)
